be00e822da28348c4b1bc66780e760f089f950b2
Nick <nickm970@gmail.com>
2015-08-09 11:42:27 -0400

    added missing comments to MailboxManager.java

--- MailboxManager.java
+++ MailboxManager.java
@@ -4,7 +4,10 @@ import java.net.DatagramSocket;
 import java.net.SocketAddress;
 import java.util.HashMap;
 
-/**
+/** Class MailboxManager provides the server program's mailbox manager. The
+ * mailbox manager keeps track of all view proxy objects, reads all incoming
+ * datagrams, and forwards each datagram to the appropriate view proxy.
+ *
  * @author Nicholas A. Mattis
  * @version 8/5/2015
  */
@@ -15,10 +18,20 @@ public class MailboxManager {
     private byte[] payload = new byte[128];
     private SessionManager sessionManager = new SessionManager();
 
+    /**
+     * Constructor
+     *
+     * @param mailbox   DatagramSocket from which to read datagrams
+     */
     public MailboxManager(DatagramSocket mailbox) {
         this.mailbox = mailbox;
     }
 
+    /**
+     * Receive and process the datagram
+     *
+     * @throws IOException  Thrown if an I/O error occurs
+     */
     public void receiveMessage() throws IOException {
         DatagramPacket packet = new DatagramPacket(payload, payload.length);
         mailbox.receive(packet);
